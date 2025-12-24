30ab44ba797b6e1314f7be4e5be001822c44122a
ayoubMO19 <ayoubweb04@gmail.com>
2025-12-24 16:49:03 +0100

    The Swagger dependency has been integrated, configured, and the endpoints have been documented. Everything has been tested

--- src/main/java/com/vexa/ecommerce/Products/ProductsController.java
+++ src/main/java/com/vexa/ecommerce/Products/ProductsController.java
@@ -3,6 +3,13 @@ package com.vexa.ecommerce.Products;
 import com.vexa.ecommerce.Categories.CategoriesRepository;
 import com.vexa.ecommerce.Products.DTOs.ProductRequestDTO;
 import com.vexa.ecommerce.Products.DTOs.ProductResponseDTO;
+import com.vexa.ecommerce.Users.DTOs.UserResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@SecurityRequirement(name = "Bearer Authentication")
 public class ProductsController {
 
     private final ProductsService productsService;
@@ -23,6 +31,13 @@ public class ProductsController {
     }
 
     // ENDPOINTS PARA ROL ADMIN
+    @Operation(
+            summary = "(ADMIN) - Crear un nuevo producto",
+            description = "Crear un nuevo producto en el sistema",
+            tags = {"Admin Endpoints"}
+    )
+    @ApiResponse(responseCode = "200", description = "Producto creado existosamente",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))) // Respuesta exitosa
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO requestDTO) {
@@ -36,6 +51,13 @@ public class ProductsController {
         return ResponseEntity.ok(ProductMapper.toDTO(savedProduct));
     }
 
+    @Operation(
+            summary = "(ADMIN) - Actualizar un producto",
+            description = "Actualizar un producto del sistema",
+            tags = {"Admin Endpoints"}
+    )
+    @ApiResponse(responseCode = "200", description = "Producto actualizado existosamente",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))) // Respuesta exitosa
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ProductResponseDTO> updateProduct(
@@ -53,6 +75,12 @@ public class ProductsController {
         return ResponseEntity.ok(ProductMapper.toDTO(savedProduct));
     }
 
+    @Operation(
+            summary = "(ADMIN) - Eliminar un producto",
+            description = "Eliminar un producto en el sistema",
+            tags = {"Admin Endpoints"}
+    )
+    @ApiResponse(responseCode = "200", description = "Producto eliminado existosamente") // Respuesta exitosa
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
@@ -62,6 +90,14 @@ public class ProductsController {
 
 
     // ENDPOINTS PARA ROL USER
+    @Tag(name = "Products", description = "Endpoints para obtener productos")
+    @Operation(
+            summary = "Obtener productos",
+            description = "Obtener todos los productos del sistema",
+            tags = {"Products"}
+    )
+    @ApiResponse(responseCode = "200", description = "Products obtenidos existosamente",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))) // Respuesta exitosa
     @GetMapping
     public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
         List<Products> products = productsService.getAllProducts();
@@ -72,6 +108,13 @@ public class ProductsController {
         return ResponseEntity.ok(dtoList);
     }
 
+    @Operation(
+            summary = "Obtener producto por id",
+            description = "Obtener producto por id",
+            tags = {"Products"}
+    )
+    @ApiResponse(responseCode = "200", description = "Producto obtenido existosamente",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))) // Respuesta exitosa
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Integer id) {
         Products product = productsService.getProductById(id);
---
+++ src/main/java/com/vexa/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.vexa.ecommerce.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("VEXA E-COMMERCE REST API")
+                        .description("This is the official VEXA Ecommerce REST API.")
+                        .version("1.0").contact(new Contact().name("ayoubMO19")
+                                .email( "ayoubmorghiouhda@gmail.com").url("https://github.com/ayoubMO19"))
+                        .license(new License().name("Apache 2.0")
+                                .url("https://www.apache.org")));
+    }
+}
