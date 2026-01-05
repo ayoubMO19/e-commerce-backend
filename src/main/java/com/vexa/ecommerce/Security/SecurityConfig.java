e73e8f01deb6209dde94688a7c6b56983b5a7fa8
ayoubMO19 <ayoubweb04@gmail.com>
2026-01-05 11:52:24 +0100

    New public route to make Swagger accessible in production

--- src/main/java/com/vexa/ecommerce/Security/SecurityConfig.java
+++ src/main/java/com/vexa/ecommerce/Security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/products").permitAll() // Permite obtener productos sin autenticación
                         .requestMatchers(HttpMethod.GET, "/api/products/{id}").permitAll() // Permite obtener producto por ID sin autenticación
                         .requestMatchers(
+                                "/",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
