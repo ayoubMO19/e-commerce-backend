d96286e39b7599ba0ad96039902e801252be25ef
ayoubMO19 <ayoubweb04@gmail.com>
2026-03-08 20:27:49 +0100

    habilitar origen vercel

--- src/main/java/com/vexa/ecommerce/Security/SecurityConfig.java
+++ src/main/java/com/vexa/ecommerce/Security/SecurityConfig.java
@@ -76,8 +76,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // Añadimos tu localhost y cualquier otra URL que necesites
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://e-commerce-frontend-mu-five.vercel.app" // URL de Vercel
+        ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
